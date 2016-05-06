39727369e1151290fd6e521d297263d6311f9c75
Bartłomiej Kołodziejczyk <bartek.kol93@gmail.com>
2016-05-07 00:58:02 +0200

    lesson2

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -7,10 +7,11 @@ namespace WinTail
     /// Actor responsible for reading FROM the console. 
     /// Also responsible for calling <see cref="ActorSystem.Shutdown"/>.
     /// </summary>
-    class ConsoleReaderActor : UntypedActor
+    public class ConsoleReaderActor : UntypedActor
     {
         public const string ExitCommand = "exit";
-        private IActorRef _consoleWriterActor;
+        public const string StartCommand = "start";
+        private readonly IActorRef _consoleWriterActor;
 
         public ConsoleReaderActor(IActorRef consoleWriterActor)
         {
@@ -19,21 +20,51 @@ namespace WinTail
 
         protected override void OnReceive(object message)
         {
-            var read = Console.ReadLine();
-            if (!string.IsNullOrEmpty(read) && String.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            if (message.Equals(StartCommand))
             {
-                // shut down the system (acquire handle to system via
-                // this actors context)
-                Context.System.Shutdown();
-                return;
+                PrintInstructions();
+            }
+            else if (message is Messages.InputError)
+            {
+                _consoleWriterActor.Tell((Messages.InputError)message);
             }
 
-            // send input to the console writer to process and print
-            // YOU NEED TO FILL IN HERE
-
-            // continue reading messages from the console
-            // YOU NEED TO FILL IN HERE
+            GetAndValidateInput();
         }
 
+        private static void PrintInstructions()
+        {
+            Console.WriteLine("Write whatever you want into the console!");
+            Console.WriteLine("Some entries will pass validation, and some won't...\n\n");
+            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+        }
+
+        private static bool IsValid(string message) => message.Length % 2 == 0;
+
+        private void GetAndValidateInput()
+        {
+            var message = Console.ReadLine();
+            if (string.IsNullOrEmpty(message))
+            {
+                Self.Tell(new Messages.NullInputError("No input received. "));
+            }
+            else if (string.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            {
+                Context.System.Shutdown();
+            }
+            else
+            {
+                if (IsValid(message))
+                {
+                    _consoleWriterActor.Tell(new Messages.InputSuccess("Thank you. Message was valid. "));
+
+                    Self.Tell(new Messages.ContinueProcessing());
+                }
+                else
+                {
+                    Self.Tell(new Messages.InputError("Input was not valid. "));
+                }
+            }
+        }
     }
 }
